563ef7220d012ade05b696ba0bd17e6525af2e4e
kipkoechke <chepkwonyke1@gmail.com>
2023-01-09 14:08:09 +0300

    Made the list scrollable

--- lib/main.dart
+++ lib/main.dart
@@ -26,21 +26,23 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Flutter App'),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
-            width: double.infinity,
-            child: Card(
-              color: Colors.blue,
-              child: Text(
-                'CHART!',
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text(
+                  'CHART!',
+                ),
+                elevation: 5.0,
               ),
-              elevation: 5.0,
             ),
-          ),
-          UserTransactions(),
-        ],
+            UserTransactions(),
+          ],
+        ),
       ),
     );
   }
