3d48ba619fc6945e622b47a093c30e14df2539a5
kipkoechke <chepkwonyke1@gmail.com>
2023-01-10 17:29:38 +0300

    Configured themes

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -24,7 +24,7 @@ class TransactionList extends StatelessWidget {
                   decoration: BoxDecoration(
                     border: Border.all(
                       width: 2.0,
-                      color: Colors.purple,
+                      color: Theme.of(context).primaryColor,
                     ),
                   ),
                   child: Text(
@@ -32,7 +32,7 @@ class TransactionList extends StatelessWidget {
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
                       fontSize: 20.0,
-                      color: Colors.purple,
+                      color: Theme.of(context).primaryColor,
                     ),
                   ),
                 ),
