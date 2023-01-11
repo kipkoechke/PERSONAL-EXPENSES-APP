362acfaf0b777c8404c4e2ffcfd86c8b66194371
kipkoechke <chepkwonyke1@gmail.com>
2023-01-11 11:11:07 +0300

    Created bars for chart

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -20,7 +20,7 @@ class Chart extends StatelessWidget {
         }
       }
       return {
-        'day': DateFormat.E().format(weekDay),
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
         'amount': totalSum,
       };
     });
@@ -31,7 +31,12 @@ class Chart extends StatelessWidget {
     return Card(
       elevation: 6.0,
       margin: EdgeInsets.all(20.0),
-      child: Row(children: []),
+      child: Row(
+        children: groupedTransactionValues.map((data) {
+          return Text('${data['day']} : ${data['amount']}');
+        }).toList(),
+      ),
+
     );
   }
 }
