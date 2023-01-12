fe7f43213710fe2222697c60f9113e031103d37f
kipkoechke <chepkwonyke1@gmail.com>
2023-01-12 10:29:26 +0300

    Rendered alternative landsape content using Switch

--- lib/main.dart
+++ lib/main.dart
@@ -72,6 +72,8 @@ class _MyHomePageState extends State<MyHomePage> {
     // )
   ];
 
+  bool _showChart = false;
+
   List<Transaction> get _recentTransactions {
     return _userTransactions.where((tx) {
       return tx.date.isAfter(
@@ -135,13 +137,29 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Container(
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Show Chart'),
+                Switch(
+                  value: _showChart,
+                  onChanged: ((value) {
+                    setState(() {
+                      _showChart = value;
+                    });
+                  }),
+                ),
+              ],
+            ),
+            _showChart
+                ? Container(
               height: (MediaQuery.of(context).size.height -
                       appBar.preferredSize.height -
                       MediaQuery.of(context).padding.top) *
                   0.25,
               child: Chart(_recentTransactions),
-            ),
+                  )
+                : 
             Container(
               height: (MediaQuery.of(context).size.height -
                       appBar.preferredSize.height -
