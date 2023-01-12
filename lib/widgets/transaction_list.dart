eefee85b9b7c68c120db1477307144c4e02dcb4c
kipkoechke <chepkwonyke1@gmail.com>
2023-01-12 18:05:19 +0300

    Extracted transaction item

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_complete_guide/models/transaction.dart';
-import 'package:intl/intl.dart';
+import '../models/transaction.dart';
+import './transaction_item.dart';
+
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
@@ -32,52 +33,12 @@ class TransactionList extends StatelessWidget {
           }))
         : ListView.builder(
             itemBuilder: (context, index) {
-              return Card(
-                elevation: 5.0,
-                margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 5.0),
-                child: ListTile(
-                  leading: CircleAvatar(
-                    radius: 30.0,
-                    child: Padding(
-                      padding: EdgeInsets.all(6.0),
-                      child: FittedBox(
-                        child: Text(
-                          '\$${transactions[index].amount}',
-                        ),
-
-                      ),
-                    ),
-                  ),
-                  title: Text(
-                    transactions[index].title,
-                    style: Theme.of(context).appBarTheme.titleTextStyle,
-                  ),
-                  subtitle: Text(
-                    DateFormat.yMMMd().format(transactions[index].date),
-                  ),
-                  trailing: MediaQuery.of(context).size.width > 460
-                      ? TextButton.icon(
-                          icon: Icon(
-                            Icons.delete,
-                            color: Theme.of(context).colorScheme.error,
-                          ),
-                          onPressed: (() =>
-                              deleteTransaction(transactions[index].id)),
-                          label: Text('Delete',
-                              style: TextStyle(
-                                color: Theme.of(context).colorScheme.error,
-                              )),
-                        )
-                      : IconButton(
-                          color: Theme.of(context).colorScheme.error,
-                          icon: Icon(Icons.delete),
-                          onPressed: (() =>
-                              deleteTransaction(transactions[index].id)),
-                  ),
-                ),
-              );
+              return TransactionItem(
+                  transaction: transactions[index],
+                  deleteTransaction: deleteTransaction);
             },
             itemCount: transactions.length,
           );
   }
 }
+
